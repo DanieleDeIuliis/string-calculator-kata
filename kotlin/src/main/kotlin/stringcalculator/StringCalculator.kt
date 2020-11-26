9a6ea4466b1dffca0ddb046408dba39c0703e912
DLU <daniele.deiuliis@gmail.com>
2020-11-26 12:40:16 +0100

    ref map + sum over sequences

--- kotlin/src/main/kotlin/stringcalculator/StringCalculator.kt
+++ kotlin/src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -4,7 +4,6 @@ class StringCalculator {
     fun add(numbersToAdd: String): Int {
         if(numbersToAdd.trim().isEmpty())
             return 0
-        val numbers = numbersToAdd.split(",")
-        return numbers[0].toInt() + numbers[1].toInt()
+        return numbersToAdd.split(",").map { it.toInt() }.sum()
     }
 }
